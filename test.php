50b99118bcb8e733a9d71cd3d69421852d56cfeb
Ger Hobbelt <ger@hobbelt.com>
2010-11-23 19:51:05 +0100

    a nice test script; originally used to test a few things PHP, but now also includes several links to 'odd corners' for testing the Combiner (direct access to JS and CSS output of that one) and a few other parts of CCMS.

---
+++ test.php
@@ -0,0 +1,59 @@
+<html>
+<body>
+<pre>
+<?php
+show('1 + "10.5"');                // $foo is float (11.5)
+show('1 + "-1.3e3"');              // $foo is float (-1299)
+show('1 + "bob-1.3e3"');           // $foo is integer (1)
+show('1 + "bob3"');                // $foo is integer (1)
+show('1 + "10 Small Pigs"');       // $foo is integer (11)
+show('4 + "10.2 Little Piggies"'); // $foo is float (14.2)
+show('"10.0 pigs " + 1');          // $foo is float (11)
+show('"10.0 pigs " + 1.0');        // $foo is float (11)     
+show('00042');                   // int: 34 (octal decoding!)
+show('"00042"');                   // int: 34 (octal decoding!)
+show('"00042" == 42');                   // int: 34 (octal decoding!)
+show('"00042" == 34');                   // int: 34 (octal decoding!)
+show('intval("00042")');         // int: 42
+show('00099');                   // int: 0 (invalid octal number!)
+show('"00099"');                   // int: 0 (invalid octal number!)
+show('intval("00099")');         // int: 99
+show('intval(\'00099\')');         // int: 99
+?>
+
+For more information on this conversion, see the Unix manual page for strtod(3).
+
+To test any of the examples in this section, cut and paste the examples and insert the following line to see what's going on:
+
+<?php
+function show($foo)
+{
+    $rv = eval('return ' . $foo . ';');
+    echo "\$foo = $foo; // \$foo is " . gettype($rv) . " ($rv)<br />\n";
+}
+?>
+
+<p>link: <a href="/c/admin/includes/fancyupload/Source/FileManager.js,../../../../../../../Language/Language.en.js,Additions.js,Uploader/Fx.ProgressBar.js,Uploader/Swiff.Uploader.js,Uploader.js"> multiple js with ../ bits inside the URL</a>
+
+<p>link: <a href="/c/admin/includes/fancyupload/dummy.js,Source/FileManager.js,Language/Language.en.js,Source/Additions.js,Source/Uploader/Fx.ProgressBar.js,Source/Uploader/Swiff.Uploader.js,Source/Uploader.js"> same: multiple js, now without the ../ bits inside the URL</a>
+
+<p>link: <a href="/c/admin/img/styles/base.css,layout.css,editor.css,sprite.css"> /c/admin/img/styles/base.css,layout.css,editor.css,sprite.css</a> -- when combiner is configured to NOT run CSS minification, 
+         the comments should exhibit that the encoding is indeed UTF-8: no screwed-up names in there, 
+         e.g. the 'Based on work by' comment line
+
+<p>link: <a href="/c/lib/includes/js/mootools.js,common.js,mocha.js"> /c/lib/includes/js/mootools.js,common.js,mocha.js </a>
+
+<p>link: <a href="/c/_install/install.css"> /c/_install/install.css </a> -- should NEVER be processed by the RewriteEngine!
+
+<p>link: <a href="/c/admin/img/styles/sprite.php"> /c/admin/img/styles/sprite.php </a>
+
+<p>link: <a href="/c/sitemap.xml"> /c/sitemap.xml </a> -- MUST spit out XML with application/xml mime type, NOT text/xml
+
+<p>link: <a href="/c/lib/modules/lightbox/resources/style.css"> /c/lib/modules/lightbox/resources/style.css </a>
+
+<p>link: <a href="/c/admin/includes/modules/translation/translation.Manage.php"> /c/admin/includes/modules/translation/translation.Manage.php </a> -- direct access to the admin translation help page
+
+<p>link: <a href="/c/_install/"> /c/_install/ </a> -- direct access to the installer (IFF it's there)
+
+
+
