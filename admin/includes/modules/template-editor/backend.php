d79d1c43977277e2e81a2f7dfd4da91d067b45f4
Ger Hobbelt <ger@hobbelt.com>
2010-11-27 21:54:49 +0100

    template editor: moved JS to end of body as per Yahoo/Google speed load suggestions.   We do this everywhere as a precursor to introducing 'lazy loading' of the JavaScript: THAT one (a future edit) should fix our tinyMCE and a few other JS load order/timing related issues in CCMS admin.

--- admin/includes/modules/template-editor/backend.php
+++ admin/includes/modules/template-editor/backend.php
@@ -103,173 +103,10 @@ if($_SESSION['ccms_userLevel']<$perm['manageTemplate'])
 		<!--[if IE]>
 			<link rel="stylesheet" type="text/css" href="../../../img/styles/ie.css" />
 		<![endif]-->
-<?php
-
-// TODO: call edit_area_compressor.php only from the combiner: combine.inc.php when constructing the edit_area.js file for the first time.
-
-?>
-		<script language="Javascript" type="text/javascript" src="../../edit_area/edit_area_full.js"></script>
-		<script type="text/javascript">
-		// initialisation
-editAreaLoader.init(
-	{
-		id:"content",
-		start_highlight:true,
-		allow_resize:'both',
-		allow_toggle:true,
-		word_wrap:true,
-		<?php echo 'language:"'.$cfg['editarea_language'].'",'; ?>
-		syntax:"html"
-	});
-		
-		editAreaLoader.init({
-			id: "example_1"	// id of the textarea to transform		
-			,start_highlight: true	// if start with highlight
-			,allow_resize: "both"
-			,allow_toggle: true
-			,word_wrap: true
-		<?php echo ',language:"'.$cfg['editarea_language'].'"'; ?>
-			,syntax:"html"
-		});
-		
-		editAreaLoader.init({
-			id: "example_2"	// id of the textarea to transform	
-			,start_highlight: true
-			,allow_toggle: false
-			,language: "en"
-			,syntax: "html"	
-			,toolbar: "search, go_to_line, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help"
-			,syntax_selection_allow: "css,html,js,php,python,vb,xml,c,cpp,sql,basic,pas,brainfuck"
-			,is_multi_files: true
-			,EA_load_callback: "editAreaLoaded"
-			,show_line_colors: true
-		});
-		
-		editAreaLoader.init({
-			id: "example_3"	// id of the textarea to transform	
-			,start_highlight: true	
-			,font_size: "8"
-			,font_family: "verdana, monospace"
-			,allow_resize: "y"
-			,allow_toggle: false
-			,language: "fr"
-			,syntax: "css"	
-			,toolbar: "new_document, save, load, |, charmap, |, search, go_to_line, |, undo, redo, |, select_font, |, change_smooth_selection, highlight, reset_highlight, |, help"
-			,load_callback: "my_load"
-			,save_callback: "my_save"
-			,plugins: "charmap"
-			,charmap_default: "arrows"
-				
-		});
-		
-		editAreaLoader.init({
-			id: "example_4"	// id of the textarea to transform		
-			//,start_highlight: true	// if start with highlight
-			//,font_size: "10"	
-			,allow_resize: "no"
-			,allow_toggle: true
-			,language: "de"
-			,syntax: "python"
-			,load_callback: "my_load"
-			,save_callback: "my_save"
-			,display: "later"
-			,replace_tab_by_spaces: 4
-			,min_height: 350
-		});
-		
-		// callback functions
-		function my_save(id, content){
-			alert("Here is the content of the EditArea '"+ id +"' as received by the save callback function:\n"+content);
-		}
-		
-		function my_load(id){
-			editAreaLoader.setValue(id, "The content is loaded from the load_callback function into EditArea");
-		}
-		
-		function test_setSelectionRange(id){
-			editAreaLoader.setSelectionRange(id, 100, 150);
-		}
-		
-		function test_getSelectionRange(id){
-			var sel =editAreaLoader.getSelectionRange(id);
-			alert("start: "+sel["start"]+"\nend: "+sel["end"]); 
-		}
-		
-		function test_setSelectedText(id){
-			text= "[REPLACED SELECTION]"; 
-			editAreaLoader.setSelectedText(id, text);
-		}
-		
-		function test_getSelectedText(id){
-			alert(editAreaLoader.getSelectedText(id)); 
-		}
-		
-		function editAreaLoaded(id){
-			if(id=="example_2")
-			{
-				open_file1();
-				open_file2();
-			}
-		}
-		
-		function open_file1()
-		{
-			var new_file= {id: "to\\ é # € to", text: "$authors= array();\n$news= array();", syntax: 'php', title: 'beautiful title'};
-			editAreaLoader.openFile('example_2', new_file);
-		}
-		
-		function open_file2()
-		{
-			var new_file= {id: "Filename", text: "<a href=\"toto\">\n\tbouh\n</a>\n<!-- it's a comment -->", syntax: 'html'};
-			editAreaLoader.openFile('example_2', new_file);
-		}
-		
-		function close_file1()
-		{
-			editAreaLoader.closeFile('example_2', "to\\ é # € to");
-		}
-		
-		function toogle_editable(id)
-		{
-			editAreaLoader.execCommand(id, 'set_editable', !editAreaLoader.execCommand(id, 'is_editable'));
-		}
-	
-
-	
-	
-function confirmation()
-{
-	var answer=confirm(<?php echo"'".$ccms['lang']['editor']['confirmclose']."'";?>);
-	if(answer)
-	{
-		try
-		{
-			parent.MochaUI.closeWindow(parent.$('sys-tmp_ccms'));
-		}
-		catch(e)
-		{
-			if (typeof top.location.replace == "function")
-			{
-				top.location.replace("<?php echo makeAbsoluteURI($cfg['rootdir'] . 'admin/index.php'); ?>");
-			}
-			else
-			{
-				top.location.href = "<?php echo makeAbsoluteURI($cfg['rootdir'] . 'admin/index.php'); ?>";
-			}
-		}
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-</script>
 	</head>
 <body>
 	<div class="module" id="template-editor">
-
-
+	
 <h2>EditArea examples</h2>
 <p>Retrieve EditArea on <a href='http://sourceforge.net/projects/editarea'>sourceforge</a> or on 
 	my personal <a href='http://www.cdolivet.com/index.php?page=editArea'>website</a>.
@@ -526,5 +363,150 @@ class Python:
 			</div>
 		</form>
 	</div>
+<?php
+
+// TODO: call edit_area_compressor.php only from the combiner: combine.inc.php when constructing the edit_area.js file for the first time.
+
+?>
+		<script language="Javascript" type="text/javascript" src="../../edit_area/edit_area_full.js" charset="utf-8"></script>
+		<script type="text/javascript" src="../../../../lib/includes/js/the_goto_guy.js" charset="utf-8"></script>
+		<script type="text/javascript">
+		// initialisation
+editAreaLoader.init(
+	{
+		id:"content",
+		start_highlight:true,
+		allow_resize:'both',
+		allow_toggle:true,
+		word_wrap:true,
+		<?php echo 'language:"'.$cfg['editarea_language'].'",'; ?>
+		syntax:"html"
+	});
+		
+		editAreaLoader.init({
+			id: "example_1"	// id of the textarea to transform		
+			,start_highlight: true	// if start with highlight
+			,allow_resize: "both"
+			,allow_toggle: true
+			,word_wrap: true
+		<?php echo ',language:"'.$cfg['editarea_language'].'"'; ?>
+			,syntax:"html"
+		});
+		
+		editAreaLoader.init({
+			id: "example_2"	// id of the textarea to transform	
+			,start_highlight: true
+			,allow_toggle: false
+			,language: "en"
+			,syntax: "html"	
+			,toolbar: "search, go_to_line, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help"
+			,syntax_selection_allow: "css,html,js,php,python,vb,xml,c,cpp,sql,basic,pas,brainfuck"
+			,is_multi_files: true
+			,EA_load_callback: "editAreaLoaded"
+			,show_line_colors: true
+		});
+		
+		editAreaLoader.init({
+			id: "example_3"	// id of the textarea to transform	
+			,start_highlight: true	
+			,font_size: "8"
+			,font_family: "verdana, monospace"
+			,allow_resize: "y"
+			,allow_toggle: false
+			,language: "fr"
+			,syntax: "css"	
+			,toolbar: "new_document, save, load, |, charmap, |, search, go_to_line, |, undo, redo, |, select_font, |, change_smooth_selection, highlight, reset_highlight, |, help"
+			,load_callback: "my_load"
+			,save_callback: "my_save"
+			,plugins: "charmap"
+			,charmap_default: "arrows"
+				
+		});
+		
+		editAreaLoader.init({
+			id: "example_4"	// id of the textarea to transform		
+			//,start_highlight: true	// if start with highlight
+			//,font_size: "10"	
+			,allow_resize: "no"
+			,allow_toggle: true
+			,language: "de"
+			,syntax: "python"
+			,load_callback: "my_load"
+			,save_callback: "my_save"
+			,display: "later"
+			,replace_tab_by_spaces: 4
+			,min_height: 350
+		});
+		
+		// callback functions
+		function my_save(id, content){
+			alert("Here is the content of the EditArea '"+ id +"' as received by the save callback function:\n"+content);
+		}
+		
+		function my_load(id){
+			editAreaLoader.setValue(id, "The content is loaded from the load_callback function into EditArea");
+		}
+		
+		function test_setSelectionRange(id){
+			editAreaLoader.setSelectionRange(id, 100, 150);
+		}
+		
+		function test_getSelectionRange(id){
+			var sel =editAreaLoader.getSelectionRange(id);
+			alert("start: "+sel["start"]+"\nend: "+sel["end"]); 
+		}
+		
+		function test_setSelectedText(id){
+			text= "[REPLACED SELECTION]"; 
+			editAreaLoader.setSelectedText(id, text);
+		}
+		
+		function test_getSelectedText(id){
+			alert(editAreaLoader.getSelectedText(id)); 
+		}
+		
+		function editAreaLoaded(id){
+			if(id=="example_2")
+			{
+				open_file1();
+				open_file2();
+			}
+		}
+		
+		function open_file1()
+		{
+			var new_file= {id: "to\\ é # € to", text: "$authors= array();\n$news= array();", syntax: 'php', title: 'beautiful title'};
+			editAreaLoader.openFile('example_2', new_file);
+		}
+		
+		function open_file2()
+		{
+			var new_file= {id: "Filename", text: "<a href=\"toto\">\n\tbouh\n</a>\n<!-- it's a comment -->", syntax: 'html'};
+			editAreaLoader.openFile('example_2', new_file);
+		}
+		
+		function close_file1()
+		{
+			editAreaLoader.closeFile('example_2', "to\\ é # € to");
+		}
+		
+		function toogle_editable(id)
+		{
+			editAreaLoader.execCommand(id, 'set_editable', !editAreaLoader.execCommand(id, 'is_editable'));
+		}
+	
+
+	
+	
+function confirmation()
+{
+	var answer = <?php echo (strpos($cfg['verify_alert'], 'X') !== false ? 'confirm("'.$ccms['lang']['editor']['confirmclose'].'")' : 'true'); ?>;
+	if(answer)
+	{
+		return !close_mochaUI_window_or_goto_url("<?php echo makeAbsoluteURI($cfg['rootdir'] . 'admin/index.php'); ?>", 'sys-tmp_ccms');
+	}
+	return false;
+}
+</script>
 </body>
 </html>
