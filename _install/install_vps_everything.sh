79b5037ec5d156685d14ef058f53c9c1828b449f
Ger Hobbelt <ger@hobbelt.com>
2010-12-25 13:23:41 +0100

    scripts adjusted to install the basics on Ubuntu Server (10.10, Maverick Meerkat)

--- _install/install_vps_everything.sh
+++ _install/install_vps_everything.sh
@@ -6,15 +6,19 @@ apt-get update
 # upgrade/update all packages currently installed on the system, forcibly upgrading EVERYTHING
 apt-get dist-upgrade --ignore-hold
 
-for pkg in `grep -v -h '#' install_vps_everything.txt` ; do
+for pkg in `grep -v -h '#' install_vps_everything.txt | tr -s '[\r\n]' ' ' ` ; do
 	# skip the newlines produced by the for+grep combo: only do real packages!
 	if test "$pkg" '>' 'a' ; then 
+		echo --- apt-get install [$pkg]
 		apt-get install $pkg
 	fi
 done
 
+# install 7zip
+./install-7zip.sh
+
 # install the packages we need for CompactCMS:
-echo ./install_apache_et_al.sh
+./install_apache_et_al.sh
 
 # and clean up the packages which were only installed temporarily during this run
 apt-get -y autoremove
