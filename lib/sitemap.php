e12231320b802fa5745c9bad985ef5d378bc4d00
Ger Hobbelt <ger@hobbelt.com>
2010-12-25 13:30:24 +0100

    scratch pad: all sources of third party libs includes; minor local edits for preference added.

--- lib/sitemap.php
+++ lib/sitemap.php
@@ -98,7 +98,7 @@ check_session_sidpatch_and_start();
 
 
 // Check first whether installation directory exists
-if(is_dir('./_install/') && is_file('./_install/index.php') && !$cfg['IN_DEVELOPMENT_ENVIRONMENT']) 
+if(is_dir('./_install/') && is_file('./_install/index.php') && !$cfg['IN_DEVELOPMENT_ENVIRONMENT'] && 0) 
 {
 	header('Location: ' . makeAbsoluteURI('./_install/index.php'));
 	exit();
