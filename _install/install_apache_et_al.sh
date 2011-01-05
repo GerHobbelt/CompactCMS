bec6ac7f65e824f5c5f406269d90b9d8d175b34e
Ger Hobbelt <ger@hobbelt.com>
2011-01-05 16:53:24 +0100

    updated shell scripts and stuff in /_install for when you want to install CompactCMS (and some other goodies) from scratch on an Ubuntu-based VPS.

--- _install/install_apache_et_al.sh
+++ _install/install_apache_et_al.sh
@@ -5,3 +5,11 @@ apt-get install -y mysql-client mysql-common mysql-server
 apt-get install -y libapache2-mod-auth-mysql 
 apt-get install -y php5-mysql php5 phpmyadmin php5-gd php5-tidy php5-cli php5-curl php5-common
 
+# install and enable the required modules:
+a2enmod rewrite deflate expires php5 vhost_alias
+
+## the diff/patch includes a change which assumes phppgadmin (for postgresql)
+## has been installed as well: hence this line is disabled here.
+#patch -lu /etc/apache2/sites-available/default diff-4-phpDBadmin.patch
+
+/etc/init.d/apache2 restart
