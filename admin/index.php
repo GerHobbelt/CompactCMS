ed6fcfcf5a089efcc8617da8b80ab8bc1a9b2f96
Ger Hobbelt <ger@hobbelt.com>
2011-01-19 08:14:47 +0100

    correction for another MSIE 6/7/8 fix: make sure only the admin index screen doesn't get extra scrollbars; all the other admin screens, when either shown as mochaUI windows or in separate tabs, /need/ their scrollbars!

--- admin/index.php
+++ admin/index.php
@@ -79,7 +79,7 @@ if ($db->ErrorNumber())
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $cfg['language']; ?>">
+<html id="admin_index_page" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $cfg['language']; ?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=8" />
