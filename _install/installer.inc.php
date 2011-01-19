cc1302afc602b2b554ef461b98c2540d438fc685
Ger Hobbelt <ger@hobbelt.com>
2011-01-19 03:54:08 +0100

    installer: used the correct upgrade flag checks; moved the upgrade process code to a separate file: it stands on its own and the installer.inc.php was becoming quite drowned by it.

--- _install/installer.inc.php
+++ _install/installer.inc.php
@@ -48,6 +48,9 @@ session_start();
 // Load generic functions
 /*MARKER*/require_once(BASE_PATH . '/lib/includes/common.inc.php');
 
+// Load upgrade helper functions
+/*MARKER*/require_once('./upgrader.inc.php');
+
 
 // Set current && additional step
 $nextstep = getPOSTparam4IdOrNumber('do');
@@ -357,11 +360,11 @@ if($nextstep == md5('4') && CheckAuth())
 			<tr class="altcolor">
 				<th width="55%" scope="row">Install Type</th>
 				<td><?php 
-				if ($_SESSION['variables']['do_upgrade'])
+				if ($do_upgrade)
 				{
 					echo '<span class="signal_upgrade_mode">Upgrade/Restore</span>';
 				}
-				else if ($_SESSION['variables']['may_upgrade'])
+				else if ($may_upgrade)
 				{
 					// we MAY but we DO NOT upgrade... hmmm...
 					echo '<span class="signal_upgrade_mode">New Installation</span>';
@@ -551,96 +554,9 @@ if($nextstep == md5('final') && CheckAuth())
 			die();
 		}
 
-		if ($err == 0 && $_SESSION['variables']['do_upgrade'])
+		if ($err == 0 && $do_upgrade)
 		{
-			$sql = file_get_contents(BASE_PATH.'/media/files/ccms-restore/compactcms-sqldump.sql');
-			$sql = preg_replace('/\\bccms_\\B/', $_SESSION['variables']['db_prefix'], $sql); // all tables here-in will get the correct prefix: we're doing a restore, so we have this info from the config.inc.php file, but we may have changed our setup in the install run just before!
-			$sql = preg_replace("/'admin', '[0-9a-f]{32}'/", "'admin', '".md5($_SESSION['variables']['userPass'].$_SESSION['variables']['authcode'])."'", $sql);
-			// note that the passwords for the other users in the backup may be invalid IFF you changed the authcode!
-			$sql = str_replace("\r\n", "\n", $sql);
-			
-			// Execute per sql piece: 
-			$currently_in_sqltextdata = false;
-			$query_so_far = '';
-			$queries = explode(";\n", $sql);
-			foreach($queries as $tok)
-			{
-				// filter query: remove comment lines, then see if there's anything left to BE a query...
-				$lines = array_filter(explode("\n", $tok), "is_a_sql_query_piece");
-				if ($currently_in_sqltextdata)
-				{
-					/*
-					 * MySQL supports multiline texts in queries; apparently we have a text here which has a line ending with a semicolon :-(
-					 * 
-					 * We can only be certain it's a b0rked query by the time we've reached the very end of the SQL file!
-					 */
-					$query_so_far .= implode("\n", $lines) . ";\n";
-					continue;
-				}
-					
-				$tok = trim($query_so_far . implode("\n", $lines));
-				$query_so_far = '';
-
-				if (empty($tok))
-					continue;
-
-				/*
-				 * - ignore 'DROP TABLE' queries
-				 * 
-				 * - process 'CREATE TABLE' queries by REPLACING them with 'TRUNCATE TABLE' queries; 
-				 *   after all, they will soon be followed up with INSERT INTO queries and we don't 
-				 *   want the 'fresh install' records to linger in there when performing 
-				 *   an upgrade/restore.
-				 *   
-				 *   NOTE that SQL dumps since 1.4.2 (rev. 2011/01/11) do contain their own TRUNCATE TABLE
-				 *   statements, and we do know that is so, but here we wish to be as backwards compatible
-				 *   as humanly possible. Besides a dual TRUNCATE TABLE doesn't hurt, so we don't filter
-				 *   those TRUNCATE statements when they exist in the original SQL script.
-				 */
-				if (preg_match('/DROP\s+TABLE/i', $tok))
-					continue;
-					
-				if (preg_match('/CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?`?([a-zA-Z0-9_\-]+)`?\s+\(/is', $tok, $matches))
-				{
-					if (!$cfg['IN_DEVELOPMENT_ENVIRONMENT'])
-					{
-						$results = $db->TruncateTable($matches[2]);
-						if ($results == false)
-						{
-							$errors[] = 'Error: executing query: ' . $db->GetLastSQL();
-							$errors[] = $db->Error();
-							$err++;
-						}
-					}
-					else
-					{
-						$sqldump[] = "Execute query:\n---------------------------------------\nTRUNCATE TABLE `" . $matches[2] . "`\n---------------------------------------\n";
-					}
-				}
-				else
-				{
-					if (!$cfg['IN_DEVELOPMENT_ENVIRONMENT'])
-					{
-						$results = $db->Query($tok);
-						if ($results == false)
-						{
-							$errors[] = 'Error: executing query: ' . $tok;
-							$errors[] = $db->Error();
-							$err++;
-						}
-					}
-					else
-					{
-						$sqldump[] = "Execute query:\n---------------------------------------\n" . $tok . "\n---------------------------------------\n";
-					}
-				}
-			}
-			
-			if ($currently_in_sqltextdata)
-			{
-				echo "<pre>B0rked on query:\n".$query_so_far."\n---------------------------------\n";
-				die();
-			}
+			$err = perform_upgrade(&$db, &$log, &$errors, &$sqldump);
 		}
 		
 		if ($err == 0)
