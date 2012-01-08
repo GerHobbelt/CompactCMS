b247705361d382a06d73dbbb2c668737daec6a4e
Ger Hobbelt <ger@hobbelt.com>
2012-01-08 22:23:32 +0100

    introduces bash shell script to streamline git submodule work.

---
+++ _install/git-submodules-foreach-task.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+#
+# this script is meant to run as the git submodule foreach command; when you
+# use this script, any errors reported by 'git push' (e.g. when you don't 
+# have push access on that particular repo) do NOT abort the 'foreach' run
+# so that you can update all repo's in one single 'foreach' command...
+#
+
+pwd
+echo arguments: $0 $@
+
+case "$1" in
+checkout)
+	echo checkout to master branch...
+	git checkout master
+	git pull --all
+	;;
+commit)	echo commit a.k.a. checkin
+	git commit
+	;;
+fetch)	echo fetch remotes...
+	git fetch --all
+	git fetch --tags
+	;;
+push)	echo push commits...
+	git push --all
+	;;
+foreach) echo Performing action on every submodule recursively...
+	shift
+	git submodule foreach --recursive $(pwd)/$0 $@
+	;;
+init) echo Initializing the submodules recursively...
+	shift
+	# so you can extra arguments like '--recursive':
+	git submodule update --init $@
+	;;
+*)
+	echo unsupported command parameter: $1
+	;;
+esac
+
+exit 0;
+
+
